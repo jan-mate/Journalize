ddc302c6885ae5f9af92c08f423f0a1c8e6dc6e9
jan-mate <killerbean69420@skiff.com>
2024-07-03 10:06:26 +0200

    Added markdown rendering + Image support

--- app/src/main/java/com/example/journal/EntryEditorActivity.kt
+++ app/src/main/java/com/example/journal/EntryEditorActivity.kt
@@ -1,10 +1,14 @@
 package com.example.journal
 
+import android.Manifest
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.provider.OpenableColumns
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
@@ -14,13 +18,24 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.gson.GsonBuilder
 import com.google.gson.reflect.TypeToken
+import io.noties.markwon.Markwon
+import io.noties.markwon.image.ImagesPlugin
+import io.noties.markwon.image.file.FileSchemeHandler
 import java.io.File
+import java.io.FileOutputStream
 import java.io.FileReader
 import java.io.FileWriter
 import java.text.SimpleDateFormat
 import java.util.*
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import androidx.exifinterface.media.ExifInterface
+
 
 class EntryEditorActivity : AppCompatActivity() {
 
@@ -28,16 +43,21 @@ class EntryEditorActivity : AppCompatActivity() {
     private lateinit var viewFilesButton: Button
     private lateinit var newEntryButton: Button
     private lateinit var lastEntryButton: Button
+    private lateinit var renderButton: Button
+    private lateinit var mediaButton: Button
     private lateinit var currentEntryTextView: TextView
+    private lateinit var renderedTextView: TextView
 
     companion object {
         var entries = mutableListOf<EntryData>()
         const val PREFS_NAME = "JournalPrefs"
         const val LAST_OPENED_TIME = "lastOpenedTime"
+        const val REQUEST_CODE_PERMISSIONS = 10
     }
 
-
     private var currentEntryId: String? = null
+    private var isEditMode = true
+    private val PICK_IMAGE_REQUEST = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,7 +67,10 @@ class EntryEditorActivity : AppCompatActivity() {
         viewFilesButton = findViewById(R.id.viewFilesButton)
         newEntryButton = findViewById(R.id.newEntryButton)
         lastEntryButton = findViewById(R.id.lastEntryButton)
+        renderButton = findViewById(R.id.renderButton)
+        mediaButton = findViewById(R.id.mediaButton)
         currentEntryTextView = findViewById(R.id.currentEntryTextView)
+        renderedTextView = findViewById(R.id.renderedTextView)
 
         viewFilesButton.setOnClickListener {
             val intent = Intent(this, EntryListActivity::class.java)
@@ -62,6 +85,18 @@ class EntryEditorActivity : AppCompatActivity() {
 
         lastEntryButton.setOnClickListener {
             openPreviousEntry()
+        }
+
+        renderButton.setOnClickListener {
+            toggleRenderMode()
+        }
+
+        mediaButton.setOnClickListener {
+            if (hasPermissions()) {
+                selectImage()
+            } else {
+                requestPermissions()
+            }
         }
 
         editText.addTextChangedListener(object : TextWatcher {
@@ -84,39 +119,30 @@ class EntryEditorActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
 
-        // Store the current time as the last opened time
         val currentTime = System.currentTimeMillis()
         val sharedPreferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
         val editor = sharedPreferences.edit()
         editor.putLong(LAST_OPENED_TIME, currentTime)
         editor.apply()
     }
-
-
 
     override fun onResume() {
         super.onResume()
 
-        // Get the current time
         val currentTime = System.currentTimeMillis()
 
-        // Retrieve the last opened time from shared preferences
         val sharedPreferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
         val lastOpenedTime = sharedPreferences.getLong(LAST_OPENED_TIME, 0)
 
-        // Check if the difference is more than 60 seconds
         if (currentTime - lastOpenedTime > 60000) {
             createNewEntry()
-            editText.text.clear()  // Clear the text in the EditText
+            editText.text.clear()
         }
 
-        // Store the current time as the last opened time
         val editor = sharedPreferences.edit()
         editor.putLong(LAST_OPENED_TIME, currentTime)
         editor.apply()
     }
-
-
 
     private fun loadEntries() {
         val jsonFile = File(filesDir, "entries_log.json")
@@ -125,7 +151,7 @@ class EntryEditorActivity : AppCompatActivity() {
                 val json = FileReader(jsonFile).use { it.readText() }
                 val gson = GsonBuilder().create()
                 val listType = object : TypeToken<List<EntryData>>() {}.type
-                entries = gson.fromJson<List<EntryData>>(json, listType).toMutableList()  // Explicitly specify type
+                entries = gson.fromJson<List<EntryData>>(json, listType).toMutableList()
                 Log.d("EntryOperation", "Loaded JSON: $entries")
             } catch (e: Exception) {
                 Log.e("EntryOperation", "Error reading JSON file", e)
@@ -229,15 +255,171 @@ class EntryEditorActivity : AppCompatActivity() {
         imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
     }
 
+    private fun hasPermissions(): Boolean {
+        val permissions = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            arrayOf(
+                Manifest.permission.READ_MEDIA_IMAGES,
+                Manifest.permission.READ_MEDIA_VIDEO,
+                Manifest.permission.READ_MEDIA_AUDIO
+            )
+        } else {
+            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+        return permissions.all { ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED }
+    }
+
+    private fun requestPermissions() {
+        val permissions = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            arrayOf(
+                Manifest.permission.READ_MEDIA_IMAGES,
+                Manifest.permission.READ_MEDIA_VIDEO,
+                Manifest.permission.READ_MEDIA_AUDIO
+            )
+        } else {
+            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE_PERMISSIONS)
+    }
+
+    private fun selectImage() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            type = "image/*"
+            putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+            addCategory(Intent.CATEGORY_OPENABLE)
+        }
+        startActivityForResult(Intent.createChooser(intent, "Select Images"), PICK_IMAGE_REQUEST)
+    }
+
+
+    private fun toggleRenderMode() {
+        if (isEditMode) {
+            renderMarkdown()
+            renderButton.text = "Edit"
+            editText.visibility = View.GONE
+            renderedTextView.visibility = View.VISIBLE
+        } else {
+            editText.visibility = View.VISIBLE
+            renderedTextView.visibility = View.GONE
+            renderButton.text = "Render"
+        }
+        isEditMode = !isEditMode
+    }
+
+    private fun renderMarkdown() {
+        val text = editText.text.toString()
+        if (text.isNotEmpty()) {
+            val markwon = Markwon.builder(this)
+                .usePlugin(ImagesPlugin.create { plugin ->
+                    plugin.addSchemeHandler(FileSchemeHandler.create())
+                })
+                .build()
+            markwon.setMarkdown(renderedTextView, text)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (grantResults.isNotEmpty() && grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+                selectImage()
+            } else {
+                // Permission denied, show a message to the user
+            }
+        }
+    }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == 1 && resultCode == RESULT_OK) {
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK) {
+            data?.let { intentData ->
+                val clipData = intentData.clipData
+                if (clipData != null) {
+                    // Multiple images selected
+                    for (i in 0 until clipData.itemCount) {
+                        val uri = clipData.getItemAt(i).uri
+                        handleImageUri(uri)
+                    }
+                } else {
+                    // Single image selected
+                    intentData.data?.let { uri ->
+                        handleImageUri(uri)
+                    }
+                }
+            }
+        } else if (requestCode == 1 && resultCode == RESULT_OK) {
             data?.getStringExtra("id")?.let { id ->
                 openEntry(id)
             }
         }
     }
 
+    private fun handleImageUri(uri: Uri) {
+        val imagePath = getPathFromUri(uri)
+        if (imagePath != null) {
+            insertImagePathToMarkdown(imagePath)
+        }
+    }
+
+    private fun getPathFromUri(uri: Uri): String? {
+        var path: String? = null
+        contentResolver.query(uri, null, null, null, null)?.use { cursor ->
+            val nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+            cursor.moveToFirst()
+            val name = cursor.getString(nameIndex)
+            val file = File(filesDir, "${System.currentTimeMillis()}_$name")
+            contentResolver.openInputStream(uri)?.use { inputStream ->
+                FileOutputStream(file).use { outputStream ->
+                    inputStream.copyTo(outputStream)
+                }
+            }
+            path = file.absolutePath
+        }
+
+        path?.let {
+            adjustImageOrientation(it)
+        }
+
+        return path
+    }
+
+    private fun adjustImageOrientation(imagePath: String) {
+        try {
+            val exif = ExifInterface(imagePath)
+            val orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)
+            val bitmap = BitmapFactory.decodeFile(imagePath)
+            val rotatedBitmap = when (orientation) {
+                ExifInterface.ORIENTATION_ROTATE_90 -> rotateBitmap(bitmap, 90f)
+                ExifInterface.ORIENTATION_ROTATE_180 -> rotateBitmap(bitmap, 180f)
+                ExifInterface.ORIENTATION_ROTATE_270 -> rotateBitmap(bitmap, 270f)
+                else -> bitmap
+            }
+
+            // Save the rotated bitmap back to the file
+            FileOutputStream(imagePath).use { out ->
+                rotatedBitmap.compress(Bitmap.CompressFormat.JPEG, 100, out)
+            }
+
+        } catch (e: Exception) {
+            Log.e("EntryOperation", "Error adjusting image orientation", e)
+        }
+    }
+
+    private fun rotateBitmap(bitmap: Bitmap, degrees: Float): Bitmap {
+        val matrix = Matrix().apply { postRotate(degrees) }
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
+    }
+
+    private fun insertImagePathToMarkdown(imagePath: String) {
+        val markdownImage = "![Image](file://$imagePath)"
+        val cursorPosition = editText.selectionStart
+        editText.text?.insert(cursorPosition, markdownImage)
+        if (!isEditMode) {
+            renderMarkdown()
+        }
+    }
 
     data class EntryData(val created: String, var modified: String, var content: String)
 }
